473ef1ca3b5aea19336e33e9c513679c68b2061c
jaywarrick <jay.w.warrick@gmail.com>
2016-04-01 13:34:59 -0500

    Update analysis of magpix data

--- AnalyzeMagPixData - 20160205.R
+++ AnalyzeMagPixData - 20160205.R
@@ -1,5 +1,22 @@
 rm(list=ls())
 library(data.table)
+library(Hmisc)
+
+myInside <- data.frame(day=c(1,3,10),IL8=c(9892,14691.7,9844.1),MCP1=c(992.5,192,442.2),VEGF=c(18020.3,20773.8,16145.3))
+myOutside <- data.frame(day=c(1,3,10),IL8=c(84.3,707,953.5),MCP1=c(6.75,8.5,42),VEGF=c(280.25,2169.375,3175.5))
+myInsideSDs <- data.frame(day=c(1,3,10),IL8=c(4080.7,4008.7,8078.4),MCP1=c(1555.6,220.9,422.2),VEGF=c(698.2,1177.9,8508.7))
+myInsideSDs <- data.frame(day=c(1,3,10),IL8=c(42.0,1174.1,124.3),MCP1=c(0.5,3,0),VEGF=c(364.2,3037.7,329.9))
+
+plot(myInside$day,myInside$IL8, xlim=c(1,10), ylim=c(1,21000), pch=20, col='green', bg='green', type='o', ylab='Median Bead Intensity [au]', xlab='Days After Implantation [days]')
+lines(myInside$day,myInside$MCP1, pch=20, col='red', bg='red', type='o')
+lines(myInside$day,myInside$VEGF, pch=20, col='blue', bg='blue', type='o')
+legend('topright', legend=c('IL8    ','MCP1    ','VEGF    '), col=c('green','red','blue'), lty=c(1,1,1), pch=c(20,20,20))
+
+plot(myOutside$day,myOutside$IL8, xlim=c(1,10), ylim=c(1,21000), pch=20, col='green', bg='green', type='o', ylab='Median Bead Intensity [au]', xlab='Days After Implantation [days]')
+lines(myOutside$day,myOutside$MCP1, pch=20, col='red', bg='red', type='o')
+lines(myOutside$day,myOutside$VEGF, pch=20, col='blue', bg='blue', type='o')
+legend('topright', legend=c('IL8    ','MCP1    ','VEGF    '), col=c('green','red','blue'), lty=c(1,1,1), pch=c(20,20,20))
+
 data <- read.csv(file='/Users/jaywarrick/Documents/MMB/Projects/Sampling Device/20151110 - Small Volume Standards.csv', stringsAsFactors=FALSE)
 data$Concentration <- (1/3)^(data$Dilution-1)
 results <- subset(data, Dilution > 0)
@@ -41,10 +58,10 @@ for(analyte in grep('Analyte', names(results)))
 {
      analyteName <- names(data)[analyte]
 
-     pdf(file=file.path('/Users/jaywarrick/Documents/MMB/Projects/Sampling Device/20151110 Plots',paste0(analyteName,'.pdf')), width=6, height=4)
+     #pdf(file=file.path('/Users/jaywarrick/Documents/MMB/Projects/Sampling Device/20151110 Plots',paste0(analyteName,'.pdf')), width=6, height=4)
      plotData(resultsMean, resultsSD, analyte=analyteName, conc=standards[[analyteName]]$conc)
      title(main=standards[[analyteName]]$name)
-     dev.off()
+     #dev.off()
 }
 
 # Get the ratio
